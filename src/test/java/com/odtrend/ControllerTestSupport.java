75f1ab76d8d0b5adacc995d8805629fd22336cc7
od <Akkessun@gmail.com>
2025-04-11 10:53:49 +0900

    test: 상품 수집 모듈 개발 (controller test 추가)

--- src/test/java/com/odtrend/ControllerTestSupport.java
+++ src/test/java/com/odtrend/ControllerTestSupport.java
@@ -1,8 +1,10 @@
 package com.odtrend;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.odtrend.applicaiton.port.in.RegisterProductUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -14,4 +16,6 @@ public class ControllerTestSupport {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    @MockBean
+    protected RegisterProductUseCase registerProductUseCase;
 }
