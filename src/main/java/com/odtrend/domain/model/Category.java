7134144c5a178904aa06a5059a8f4f671978d770
od <Akkessun@gmail.com>
2025-07-24 15:15:58 +0900

    feat: 추천상품 등록 스케쥴러 개발 (Category 도메인 수정)

--- src/main/java/com/odtrend/domain/model/Category.java
+++ src/main/java/com/odtrend/domain/model/Category.java
@@ -1,5 +1,24 @@
 package com.odtrend.domain.model;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum Category {
-    FASHION, DIGITAL, FOOD, LIFE;
+
+    DIGITAL("전자제품"),
+    FASHION("패션"),
+    SPORTS("스포츠"),
+    FOOD("식품"),
+    LIFE("생활용품");
+
+    private final String description;
+
+    public String description() {
+        return description;
+    }
+    
+    public static List<Category> getCategories() {
+        return List.of(Category.values());
+    }
 }
